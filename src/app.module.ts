b0177362216171ea0f0ddfed3ff6ad805021d66f
Santiago Ramos <santiagodotsh@gmail.com>
2024-09-10 16:39:03 -0500

    show images with endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
-import { join } from 'path'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { ServeStaticModule } from '@nestjs/serve-static'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ProductsModule } from './products/products.module'
 import { CommonModule } from './common/common.module'
@@ -11,9 +9,6 @@ import { FilesModule } from './files/files.module'
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname,'..','public')
-    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
