e90b65558c7e678f3daa4b3dfd3b13de12cb07e7
Santiago Ramos <santiagodotsh@gmail.com>
2024-09-13 13:40:28 -0500

    emit message only to sending client

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,11 +1,13 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer
 } from '@nestjs/websockets'
 import { Server, Socket } from 'socket.io'
 import { MessagesWsService } from './messages-ws.service'
+import { NewMessageDto } from './dto/new-message.dto'
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -24,5 +26,10 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     this.messagesWsService.removeClient(client.id)
 
     this.webSocketServer.emit('clients-updated', this.messagesWsService.getConnectedClients())
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, { message }: NewMessageDto) {
+    client.emit('message-from-server', { fullName: 'gatito', message })
   }
 }
